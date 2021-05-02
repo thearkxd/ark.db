99b37abb064b77e4df6cb2b5c0e85c551f3661e0
Theark <59927799+thearkxd@users.noreply.github.com>
2021-05-02 17:58:36 +0300

    Update Database.js

--- src/Database.js
+++ src/Database.js
@@ -5,50 +5,49 @@ const { _set, _get } = require("./Utils/Util");
 const Error = require("./Error");
 
 module.exports = class Database {
+  /** @type { String } @private */
   #dbFilePath;
+  /** @type { Object } @private */
   #jsonData;
 
-  /*
+  /**
+   * @param { String } file 
    * @constructor
-   * @param {string} file - Database file path
-   * */
+   */
   constructor(file = "arkdb.json") {
-    if (typeof file !== "string") {
-      throw new Error("Please specify a valid database name!");
-    }
-
+    if (typeof file !== "string") throw new Error("Please specify a valid database name!");
     this.#dbFilePath = file.endsWith(".json") ? `${process.cwd()}/${file}` : `${process.cwd()}/${file}.json`;
     this.#jsonData = {};
 
-    if(existsSync(this.#dbFilePath)) {
+    if (existsSync(this.#dbFilePath)) {
       this.#jsonData = this.read();
     } else {
       writeFileSync(this.#dbFilePath, "{}", "utf-8");
     }
   }
 
-  /*
-   * @param {string} key
-   * @return {*}
-   * */
+  /**
+   * @param { String } key
+   * @returns { any }
+   */
   get(key) {
     return _get(key, this.#jsonData) || null;
   }
 
-  /*
-   * @param {string} key
-   * @return {boolean}
-   * */
+  /**
+   * @param { String } key
+   * @returns { Boolean }
+   */
   has(key) {
     if (!key || typeof key !== "string") throw new Error("Please specify a valid key!");
     return !!(_get(key, this.#jsonData));
   }
 
-  /*
-   * @param {string} key
-   * @param {*} value
-   * @return {*}
-   * */
+  /**
+   * @param { String } key
+   * @param { any } value
+   * @returns { any }
+   */
   set(key, value) {
     if (!key || typeof key !== "string") throw new DbError("Please specify a valid key!");
     if (!value) throw new Error("Please specify a valid value!");
@@ -58,24 +57,24 @@ module.exports = class Database {
     return _get(key.split(".")[0], this.#jsonData);
   }
 
-  /*
+  /**
    * Writes current JSON data to database file.
-   * */
+   */
   write() {
     writeFileSync(this.#dbFilePath, JSON.stringify(this.#jsonData, null, 2));
   }
 
-  /*
-   * @param {string} key
-   * @return {boolean}
-   * */
+  /**
+   * @param { String } key
+   * @returns { Boolean }
+   */
   delete(key) {
     if (!key || typeof key !== "string") throw new Error("Please specify a valid key!");
 
-    let locations = key.split(".");
-    for(var i = 0; i < locations.length-1; i++) {
+    const locations = key.split(".");
+    for (var i = 0; i < locations.length-1; i++) {
       this.#jsonData = this.#jsonData[locations[i]] || {};
-      if(Object.keys(this.#jsonData).length === 0) return false;
+      if (Object.keys(this.#jsonData).length === 0) return false;
     }
 
     delete this.#jsonData[locations[locations.length - 1]];
@@ -83,61 +82,57 @@ module.exports = class Database {
     return true;
   }
 
-  /*
-   * @param {string} key
-   * @param {number} count
-   * @return {number}
-   * */
+  /**
+   * @param { String } key
+   * @param { Number } count
+   * @returns { Number }
+   */
   add(key, count) {
     if (!key || typeof key !== "string") throw new Error("Please specify a valid key!");
     if (!count || typeof count === "string") throw new Error("Please specify a valid count!");
-
-    var data = this.#jsonData[key] || 0;
-
+    const data = this.#jsonData[key] || 0;
     if (isNaN(data)) throw new Error("Data is not a number");
     this.set(key, data + count);
-    return (data+count);
+    return (data + count);
   }
 
-  /*
-   * @param {string} key
-   * @param {number} count
-   * @return {number}
-   * */
+  /**
+   * @param { String } key
+   * @param { Number } count
+   * @returns { Number }
+   */
   subtract(key, count) {
     if (!key || typeof key !== "string") throw new Error("Please specify a valid key!");
     if (!count || typeof count === "string") throw new Error("Please specify a valid count!");
-
-    var data = this.#jsonData[key] || 0;
-
+    const data = this.#jsonData[key] || 0;
     if (isNaN(data)) throw new Error("Data is not a number");
     this.set(key, data - count);
     return (data-count);
   }
 
-  /*
-   * @param {string} key
-   * @param {*} el
-   * @return {*}
-   * */
+  /**
+   * @param { String } key
+   * @param { any } el
+   * @returns { any }
+   */
   push(key, el) {
-    var data = this.#jsonData[key] || [];
-    if (!Array.isArray(data)) throw new Error("Data is not a array");
+    const data = this.#jsonData[key] || [];
+    if (!Array.isArray(data)) throw new Error("Data is not an array");
     data.push(el);
     this.set(key, data);
     return data;
   }
 
-  /*
-   * @return {object}
-   * */
+  /**
+   * @returns { Object }
+   */
   all() {
     return this.#jsonData;
   }
 
-  /*
-   * @return {boolean}
-   * */
+  /**
+   * @returns { Boolean }
+   */
   clear() {
     this.#jsonData = {};
     this.write();
@@ -145,12 +140,9 @@ module.exports = class Database {
   }
 
   /*
-   * @return {object}
-   * */
+   * @returns { Object }
+   */
   read() {
-    return JSON.parse(
-      readFileSync(this.#dbFilePath, { encoding: 'utf-8' }) || '{}'
-    );
+    return JSON.parse(readFileSync(this.#dbFilePath, { encoding: 'utf-8' }) || '{}');
   }
-}
-
+};
